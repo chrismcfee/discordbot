28c03b5b32f7f81cd8fd469ab9387ce8ee559384
Raina <raina@autistici.org>
2019-12-06 17:40:18 +0000

    add main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+        "github.com/bwmarrin/discordgo"
+	)
+
+	// our main function
+	func main() {
+	
+	d, err := discordgo.New("Bot <your token here>")
+
+	}
+
+	// our command handler function
+	func handleCmd() {
+	}
+}
